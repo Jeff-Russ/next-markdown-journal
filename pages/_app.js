0a2d6504a1794e8d145731527a2ba9dfdd3c9a62
Jeffrey Russ <jeffreylynnruss@gmail.com>
2021-12-12 13:23:13 -0500

    Add Tailwind styles to Header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
-import Header from '../components/Header'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Header />
-      <main className="container">
-        <Component {...pageProps} />
-      </main>
-    </>
-  )
+  return <Component {...pageProps} />
 }
