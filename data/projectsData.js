aa0a4ce3394a1bef388b9bf54d21fcf9deb93e4f
Jeffrey Russ <jeffreylynnruss@gmail.com>
2022-05-05 18:08:13 -0400

    Add this project to projects page.

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,17 +1,15 @@
 const projectsData = [
   {
+    title: 'Next Markdown Journal',
+    description: `A personal or professional journal/posts starter build on Next.js, a React framework, with Tailwind CSS and content provided as markdown or MDX files by one or more authors. It is adapted from Tailwind Nextjs Starter Blog and was used to build this site.`,
+    imgSrc: '/static/images/next-markdown-journal-homepage.png',
+    href: 'https://github.com/Jeff-Russ/next-markdown-journal',
+  },
+  {
     title: 'Adblib Blaster aka JuceOPLVSTi',
     description: `AdblibBlaster AUi a port for macOS of JuceOPLVSTi, and emulation of the AdLib FM synthesis soundcard.`,
     imgSrc: '/static/images/adlibblaster-vst.jpg',
     href: 'https://bsutherland.github.io/JuceOPLVSTi/',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/posts/the-time-machine',
   },
 ]
 
