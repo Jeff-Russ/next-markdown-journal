f649d4af13ef681d5c38c985f43baddd2ec08b4b
Jeffrey Russ <jeffreylynnruss@gmail.com>
2022-08-14 17:57:17 -0400

    add PostHog analytics. synced w/timlrx v1.5.6+ (2022-07-22)

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -27,6 +27,7 @@ const siteMetadata = {
     simpleAnalytics: false, // true or false
     umamiWebsiteId: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, // e.g. UA-000000-2 or G-XXXXXXX
+    posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
